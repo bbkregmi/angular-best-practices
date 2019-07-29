a79d6c902abaa562dc07ac5e817ccec2ac730d56
Bibek Regmi <bibek.regmi@cloudistics.com>
2019-07-29 11:06:43 -0400

    Added more modules to increase app size

--- src/app/change-detection/on-push-strategy/on-push.module.ts
+++ src/app/change-detection/on-push-strategy/on-push.module.ts
@@ -1,31 +1,40 @@
-import { Routes, RouterModule } from "@angular/router";
 import { OnPushContainerComponent } from './on-push-container.component';
 import { NgModule } from '@angular/core';
 import { WithoutOnPushModule } from './without-on-push/without-on-push.module';
 import { WithOnPushModule } from './with-on-push/with-on-push.module';
 import { CommonModule } from '@angular/common';
 import { OnPushDomModule } from './with-on-push-dom/on-push-dom.module';
-
-const onPushRoutes: Routes = [
-  {
-    path: '',
-    component: OnPushContainerComponent
-  }
-];
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule, MatMenuModule, MatToolbarModule, MatIconModule, MatCardModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { CdkStepperModule } from '@angular/cdk/stepper';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(onPushRoutes),
     WithoutOnPushModule,
     WithOnPushModule,
     OnPushDomModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    CdkTableModule,
+    CdkAccordionModule,
+    CdkTreeModule,
+    CdkStepperModule,
   ],
   declarations: [
     OnPushContainerComponent
   ],
-  exports: [
-    RouterModule
-  ]
 })
 export class OnPushModule {}
